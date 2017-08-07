e63c523bb949333d01f560dd2542a5b00e92490c
Burak <nburaktaban@gmail.com>
2017-08-06 22:24:44 -0400

    Data added

---
+++ src/app/data/products.ts
@@ -0,0 +1,12 @@
+export const carpets = [
+    { image: '0053A_CREAM',       name: 'Cream' },
+    { image: '0053A_NAVI',        name: 'Navi'},
+    { image: '0053A_RED',         name: 'Red'},
+    { image: '0069B_CREAM',       name: 'Cream'},
+    { image: '0069B_CREAM_GREEN', name: 'Cream Green'},
+    { image: '0069B_CREAM_NAVI',  name: 'Cream Navi'},
+    { image: '0069B_CREM_RED',    name: 'Cream Red'},
+    { image: '2050A_CREAM',       name: 'Cream'},
+    { image: '2050A_NAVİ',        name: 'Navi'},
+    { image: '2050A_RED',         name: 'Red'},
+]
